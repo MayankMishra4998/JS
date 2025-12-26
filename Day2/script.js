20cde7caf2b31be7db2859a086906e62a1b09968
Mayank Mishra <mi5294759@gmail.com>
2025-12-26 08:32:51 +0530

    Add files via upload

---
+++ Day2/script.js
@@ -0,0 +1,9 @@
+document.getElementById("demo").innerHTML="<h2>My Name is Mayank Mishra</h2>"
+
+document.getElementById("demo2").innerText="this is innerText property"
+
+document.writeln(9+3);
+
+alert("You Want to enter the Web Site")
+
+console.log("This show the value in console");
