f3cd0b3039b68fb63148235f8cf4a46ebd640c41
Mayank Mishra <mi5294759@gmail.com>
2025-12-27 05:04:14 +0530

    Add files via upload

---
+++ Day 3/script.js
@@ -0,0 +1,31 @@
+/*Syntax Rules
+Syntax are the rules how programs must be constructed:
+
+How to Declare variables:
+*/
+let x = 5;
+let y = 6;
+
+// How to Compute values:
+let z = x + y;
+
+console.log(z);
+
+
+// Statement :-
+
+function statement() {
+    let a, c, b
+    a = 3
+    b = 4
+    c = a + b
+}
+
+// In this function we have 4 statement's
+
+
+/*
+// Commit's
+
+ Single line commite{ //}
+ multipal line commite{ /* */}
