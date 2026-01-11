59fed868ef20c7f03240fb29324753465a55dcf3
Mayank Mishra <mi5294759@gmail.com>
2026-01-12 00:18:17 +0530

    Add files via upload

---
+++ Day 6/day6.js
@@ -0,0 +1,54 @@
+// JavaScript Operators:-
+
+
+/*
+Type's of js operator:-
+
+There are different types of JavaScript operators:
+   1 => Arithmetic Operators
+   2 => Assignment Operators
+   3 => Comparison Operators
+   4 => Logical Operators
+        And more ...
+
+ */
+
+
+// Adding Strings and Numbers :- 
+
+let name = "Mayank";
+let lastName = " Mishra"
+let age = 19;
+let sum = name + age;
+// Adding strings :- 
+let fullName = name + lastName;
+
+
+/*
+  Some more...
+        x = y  means	x = y
+        x += y  means	x = x + y
+        x -= y  means	x = x - y
+        x *= y  means	x = x * y
+        x /= y  means	x = x / y
+        x %= y  means	x = x % y
+        x **= y means	x = x ** y
+
+
+  Some important ans confusing .. 
+
+      [==]  equal to	[ x == 5]
+      [===]  equal value and equal type	 [ x === 5 ]
+      [!=	]  not equal	[ x != 5]
+      [!==]  not equal value or not equal type	[ x !== 5 ]
+*/
+
+
+/* 
+JavaScript Logical Operators  :- 
+
+ 1 => && {and Operator}  => Both condition should be true
+ 2 => || {Or Operator} => atleast one condtion should be true	  
+ 3 => ! {Not Operator}	 => value will reverse {True become's false and false becomes true}
+
+*/
